ff77b05f091b108f1c85a94c173e096f1cbaf6d4
MarianoGirbau <marianogirbau@gmail.com>
2025-06-08 20:14:56 -0300

    Editado form modal, para no permitir caracteres no definidos

--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -32,36 +32,65 @@ const EditContactModal = ({ show, handleClose, contact, onSave }) => {
               name="nombre"
               value={editedContact.nombre}
               onChange={handleChange}
+              onKeyPress={(e) => {
+                if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]$/.test(e.key)) {
+                  e.preventDefault();
+                }
+              }}
               required
+              maxLength={30}
             />
           </Form.Group>
+
           <Form.Group className="mb-3">
             <Form.Label>Apellido</Form.Label>
             <Form.Control
               name="apellido"
               value={editedContact.apellido}
               onChange={handleChange}
+              onKeyPress={(e) => {
+                if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]$/.test(e.key)) {
+                  e.preventDefault();
+                }
+              }}
               required
+              maxLength={30}
             />
           </Form.Group>
+
           <Form.Group className="mb-3">
             <Form.Label>Provincia</Form.Label>
             <Form.Control
               name="provincia"
               value={editedContact.provincia}
               onChange={handleChange}
+              onKeyPress={(e) => {
+                if (!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]$/.test(e.key)) {
+                  e.preventDefault();
+                }
+              }}
               required
+              maxLength={30}
             />
           </Form.Group>
+
           <Form.Group className="mb-3">
             <Form.Label>Teléfono</Form.Label>
             <Form.Control
               name="telefono"
               value={editedContact.telefono}
               onChange={handleChange}
+              onKeyPress={(e) => {
+                if (!/[0-9]/.test(e.key)) {
+                  e.preventDefault();
+                }
+              }}
               required
+              maxLength={10}
+              inputMode="numeric"
             />
           </Form.Group>
+
           <div className="d-flex justify-content-end">
             <Button variant="secondary" onClick={handleClose} className="me-2">
               Cancelar
@@ -73,6 +102,9 @@ const EditContactModal = ({ show, handleClose, contact, onSave }) => {
         </Form>
       </Modal.Body>
     </Modal>
+
+
+
   );
 };
 
